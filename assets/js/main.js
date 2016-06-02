4dca6e14f54b87a3b756d21804cca146ed03e210
Damon <damon.kenul@gmail.com>
2016-06-01 23:37:21 -0400

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 (function() {
-  $('.bosses > li').click(function(e) {
+  $('.bosses > li').click(function() {
     $($(this).children()[1]).toggle();
+  })
+
+  $('#search').on('input', function() {
+    Array.prototype.slice.call($('.bosses > li')).forEach(function(el) {
+      var seg = $(this).val().toLowerCase();
+      var target = $($(el).children()[0]).children()[1].textContent.toLowerCase();
+      target.includes(seg) ? $(el).show() : $(el).hide()
+    }.bind(this))
   })
 })()
